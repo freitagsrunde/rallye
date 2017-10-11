0904f32f9f977c28e6b32addbe53e84617c87483
phiphi282 <32705812+phiphi282@users.noreply.github.com>
2017-10-11 20:10:37 +0200

    Found out that SOMEONE just pasted their api key wrong

--- js/map.js
+++ js/map.js
@@ -12,26 +12,18 @@ function googleError() {
 //
 //}
 
-//var map; //<-- This is now available to both event listeners and the initialize() function
-//function initMap() {
-//	var mapOptions = {
-//		center: new google.maps.LatLng(52.512814, 13.325160),
-//		zoom: 15,
-//		mapTypeId: google.maps.MapTypeId.ROADMAP
-//	};
-//	map = new google.maps.Map(document.getElementById("map"),
-//		mapOptions);
-//	google.maps.event.addDomListener(window, 'load', initMap);
-//	google.maps.event.addDomListener(window, "resize", function() {
-//		var center = map.getCenter();
-//		google.maps.event.trigger(map, "resize");
-//		map.setCenter(center); 
-//	});}
-
+var map; //<-- This is now available to both event listeners and the initialize() function
 function initMap() {
-var uluru = {lat: 52.512814, lng: 13.325160};
-var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 10,
-  center: berlin
-});
-}
+	var mapOptions = {
+		center: new google.maps.LatLng(52.512814, 13.325160),
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById("map"),
+		mapOptions);
+	google.maps.event.addDomListener(window, 'load', initMap);
+	google.maps.event.addDomListener(window, "resize", function() {
+		var center = map.getCenter();
+		google.maps.event.trigger(map, "resize");
+		map.setCenter(center); 
+	});}
