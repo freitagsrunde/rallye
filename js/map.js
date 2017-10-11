91f0145bb9689327fc393a6733033b9dbfe82303
phiphi282 <32705812+phiphi282@users.noreply.github.com>
2017-10-11 19:21:02 +0200

    Update map.js
    trying out new map function

--- js/map.js
+++ js/map.js
@@ -12,18 +12,26 @@ function googleError() {
 //
 //}
 
-var map; //<-- This is now available to both event listeners and the initialize() function
+//var map; //<-- This is now available to both event listeners and the initialize() function
+//function initMap() {
+//	var mapOptions = {
+//		center: new google.maps.LatLng(52.512814, 13.325160),
+//		zoom: 15,
+//		mapTypeId: google.maps.MapTypeId.ROADMAP
+//	};
+//	map = new google.maps.Map(document.getElementById("map"),
+//		mapOptions);
+//	google.maps.event.addDomListener(window, 'load', initMap);
+//	google.maps.event.addDomListener(window, "resize", function() {
+//		var center = map.getCenter();
+//		google.maps.event.trigger(map, "resize");
+//		map.setCenter(center); 
+//	});}
+
 function initMap() {
-	var mapOptions = {
-		center: new google.maps.LatLng(52.512814, 13.325160),
-		zoom: 15,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	map = new google.maps.Map(document.getElementById("map"),
-		mapOptions);
-	google.maps.event.addDomListener(window, 'load', initMap);
-	google.maps.event.addDomListener(window, "resize", function() {
-		var center = map.getCenter();
-		google.maps.event.trigger(map, "resize");
-		map.setCenter(center); 
-	});}
+var uluru = {lat: 52.512814, lng: 13.325160};
+var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 10,
+  center: berlin
+});
+}
